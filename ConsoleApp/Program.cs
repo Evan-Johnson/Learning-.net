ba72250828e77d18025c4212a019f1f4e6ad27f4
Evan Johnson <johnsonevanpatrick@gmail.com>
2024-10-21 12:42:16 -0600

    Initializing .net training

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, Worst");
+using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
